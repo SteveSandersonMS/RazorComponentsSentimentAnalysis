dfcf412416ed50073a2eecbf3aedb784bd1f6abb
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2019-02-28 11:16:42 +0000

    Clean working state

---
+++ RazorComponentsSentimentAnalysis/Services/Sentiment.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.ML.Legacy;
+
+namespace RazorComponentsSentimentAnalysis.Services
+{
+    // Model input
+    public class SourceData
+    {
+        public string SentimentText { get; set; }
+    }
+
+    // Model output
+    public class Prediction
+    {
+        public float Probability { get; set; } // 0=bad, 1=good
+        public float Percentage => Probability * 100;
+    }
+
+    public static class Sentiment
+    {
+        static PredictionModel<SourceData, Prediction> model
+            = PredictionModel.ReadAsync<SourceData, Prediction>("SentimentModel.zip").Result;
+
+        public static Prediction Predict(string text)
+        {
+            return model.Predict(new SourceData { SentimentText = text });
+        }
+    }
+}
